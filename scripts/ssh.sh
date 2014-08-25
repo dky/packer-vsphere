7debb8692287cf8cb00219f13ddcdd80824452fb
Don Ky <don.d.ky@gmail.com>
2014-08-25 11:06:29 -0400

    Vagrant file don't validate md5 on iso, spacing corrections on sudo.sh

--- scripts/ssh.sh
+++ scripts/ssh.sh
@@ -4,8 +4,8 @@ SSH_FILE=/etc/ssh/sshd_config
 
 if egrep "UseDNS no" $SSH_FILE
 then
-        echo "UseDNS parameter Already exist"
+	echo "UseDNS parameter Already exist"
 else
-        echo "Turning off name resolution in SSH"
-   sed -i 's/#UseDNS yes/UseDNS no/' $SSH_FILE
+	echo "Turning off name resolution in SSH"
+	sed -i 's/#UseDNS yes/UseDNS no/' $SSH_FILE
 fi
--- scripts/sudo.sh
+++ scripts/sudo.sh
@@ -4,10 +4,10 @@ SUDO_FILE=/etc/sudoers
 
 if egrep -i "vagrant" $SUDO_FILE
 then
-   echo "SUDO entries Already exist"
+	echo "SUDO entries Already exist"
 else
 	echo "Configuring SUDO for vagrant"
-   sed -i "/root\s*ALL=(ALL)/ a\vagrant ALL=NOPASSWD:ALL" /etc/sudoers
-   sed -i 's/requiretty/\!requiretty/' /etc/sudoers
-   echo Defaults env_keep=“SSH_AUTH_SOCK” >> /etc/sudoers
+	sed -i "/root\s*ALL=(ALL)/ a\vagrant ALL=NOPASSWD:ALL" /etc/sudoers
+	sed -i 's/requiretty/\!requiretty/' /etc/sudoers
+	echo Defaults env_keep=“SSH_AUTH_SOCK” >> /etc/sudoers
 fi
